db4cc624de2bb1d54f9b349cb754ad9e0aeeafb1
denizderK <denizderya8668@gmail.com>
2023-04-26 01:14:46 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from "./pages/Main";
+import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UpdateProduct from "./pages/UpdateProduct";
+
+function App() {
+
+  
+
+  return (
+    <div className="bg-light app">
+      {/* <Navbar/>
+      <Main/>
+      <About/>
+      <NewProduct/>
+      <ProductList/> */}
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/new-product" element={<NewProduct/>} />
+        <Route path="/product-list" element={<ProductList/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/update-product" element={<UpdateProduct/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
+import axios from "axios";
+
+
+
+const ProductList = () => {
+const url=process.env.REACT_APP_API_URL;
+    const [product,setProduct] =useState([])
+    const [loading,setLoading] =useState(true)
+    const [errorState,setErrorState] =useState(false)
+
+    const getProducts = async() =>{
+        try {
+            const {data} = await axios(url)
+            setProduct(data)
+            setErrorState(false)
+            setLoading(false)
+
+        } catch (error) {
+           console.log(error)
+           setErrorState(true) 
+        }
+    }
+    console.log(product)
+
+useEffect(()=>{
+getProducts();
+},[]);
+
+
+  return (
+    <div className="container mt-3">
+      <div className={"bg-light d-sm-block d-md-flex"}>
+       {loading ? <p className="text-center text-danger w-100">Loading....</p>
+       : product.length > 0 ?
+        <>
+          <article id="product-panel" className="col-md-5">
+            <ProductCard />
+          </article>
+          <article className="col-md-5 m-3">
+            <CardTotal />
+          </article>
+        </>
+        : (!errorState && <p className="text-center text-danger w-100">No products data...</p> )
+        }
+
+       {errorState && (<p className="text-center text-danger w-100">Error..</p>)}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
