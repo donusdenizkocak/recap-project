65dab5ba7756a02fbde1a3c9b2c610667a362f6c
denizderK <denizderya8668@gmail.com>
2023-05-06 23:55:16 +0300

    edt

--- src/components/CardTotal.jsx
+++ src/components/CardTotal.jsx
@@ -5,7 +5,8 @@ const shipping = 25;
 
 const CardTotal = ({product}) => {
   
-  const subTotal =product.reduce((acc,product)=> product.price*0.9*product)
+  const subTotal=product.reduce((acc,product)=> product.ProductPrice*0.9*product.ProductQuantity + acc,0)
+  console.log(subTotal)
   
   return (
     <table className="table w-100">
@@ -13,27 +14,26 @@ const CardTotal = ({product}) => {
         <tr className="text-end">
           <th className="text-start">Subtotal</th>
           <td>
-            $<span className="subtotal"></span>
+            $<span className="subtotal">{subTotal.toFixed(2)}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Tax(18%)</th>
           <td>
-            $<span className="tax"></span>
+            $<span className="tax">{(subTotal*taxRate).toFixed(2)}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Shipping</th>
           <td>
-            $<span className="shipping"></span>
+            $<span className="shipping">{shipping}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Total</th>
           <td>
             $
-            <span className="total">
-            </span>
+            <span className="total">{(subTotal + shipping + taxRate*subTotal).toFixed(2)}</span>
           </td>
         </tr>
       </tbody>
